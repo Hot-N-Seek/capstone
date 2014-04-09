b15e3735c2a41d6e73f119a4d8f6b418131f0767
cameronholland90 <cameron@thenearsky.com>
2014-04-09 14:08:07 -0500

    added more users to database seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder {
 		Eloquent::unguard();
 
 		$this->call('UserTableSeeder');
+		$this->call('ItemTableSeeder');
 	}
 
 }
@@ -29,6 +30,186 @@ class UserTableSeeder extends Seeder {
         $user->first_name = 'Cameron';
         $user->last_name = 'Holland';
         $user->role = 'Admin';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'john@example.com';
+        $user->password = 'johnsmith';
+        $user->username = 'jsmith';
+        $user->first_name = 'John';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'bob@example.com';
+        $user->password = 'bobsmith';
+        $user->username = 'bsmith';
+        $user->first_name = 'Bob';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'alex@example.com';
+        $user->password = 'alexsmith';
+        $user->username = 'asmith';
+        $user->first_name = 'Alex';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'carl@example.com';
+        $user->password = 'carlsmith';
+        $user->username = 'csmith';
+        $user->first_name = 'Carl';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'dave@example.com';
+        $user->password = 'davesmith';
+        $user->username = 'dsmith';
+        $user->first_name = 'Dave';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'eric@example.com';
+        $user->password = 'ericsmith';
+        $user->username = 'esmith';
+        $user->first_name = 'Eric';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'fred@example.com';
+        $user->password = 'fredsmith';
+        $user->username = 'fsmith';
+        $user->first_name = 'Fred';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'gary@example.com';
+        $user->password = 'garysmith';
+        $user->username = 'gsmith';
+        $user->first_name = 'Gary';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'hal@example.com';
+        $user->password = 'halsmith';
+        $user->username = 'hsmith';
+        $user->first_name = 'Hal';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'issac@example.com';
+        $user->password = 'issacsmith';
+        $user->username = 'ismith';
+        $user->first_name = 'Issac';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'ken@example.com';
+        $user->password = 'kensmith';
+        $user->username = 'ksmith';
+        $user->first_name = 'Ken';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'larry@example.com';
+        $user->password = 'larrysmith';
+        $user->username = 'lsmith';
+        $user->first_name = 'Larry';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'mark@example.com';
+        $user->password = 'marksmith';
+        $user->username = 'msmith';
+        $user->first_name = 'Mark';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'nathan@example.com';
+        $user->password = 'nathansmith';
+        $user->username = 'nsmith';
+        $user->first_name = 'Nathan';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'omar@example.com';
+        $user->password = 'omarsmith';
+        $user->username = 'osmith';
+        $user->first_name = 'Omar';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'patrick@example.com';
+        $user->password = 'patricksmith';
+        $user->username = 'psmith';
+        $user->first_name = 'Patrick';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'quentin@example.com';
+        $user->password = 'quentinsmith';
+        $user->username = 'qsmith';
+        $user->first_name = 'Quentin';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'ray@example.com';
+        $user->password = 'raysmith';
+        $user->username = 'rsmith';
+        $user->first_name = 'Ray';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'steve@example.com';
+        $user->password = 'stevesmith';
+        $user->username = 'ssmith';
+        $user->first_name = 'Steve';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
+        $user->save();
+
+        $user = new User();
+        $user->email = 'tim@example.com';
+        $user->password = 'timsmith';
+        $user->username = 'tsmith';
+        $user->first_name = 'Tim';
+        $user->last_name = 'Smith';
+        $user->role = 'User';
         $user->save();
     }
 
