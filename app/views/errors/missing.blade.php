18d124c51e818b0d6cbeb48615b801f83072ffaa
cameronholland90 <cameron@thenearsky.com>
2014-04-10 00:45:13 -0500

    editing on css for views/took out inline styling

--- app/views/errors/missing.blade.php
+++ app/views/errors/missing.blade.php
@@ -1,6 +1,38 @@
-@extends('layouts.child')
+@extends('layouts.master')
 
 @section('content')
-    	<h1>THIS IS NOT THE PAGE YOU ARE LOOKING FOR!</h1>
+	<!-- HOME -->
+	<section id="home" class="homeTop nofull imageOnly color">
+		
+		<img src="assets/images/preview/slider/MapGeoLocator.jpg" alt="Large Logo Image" id='large-logo'/>
+
+	</section>
+	<!-- /HOME -->
+
+	<!-- HEADER -->
+	<header id="header">
+		<div class="container">
+
+			<!-- LOGO -->
+			<a href="#home" class="pull-left scrollTo"><img src="assets/images/HotnColdLogo.png" height="60" alt="welcome" /></a>
+
+			<!-- MOBILE MENU -->
+			<button id="mobileMenu" class="fa fa-bars" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse"></button>
+
+			<!-- NAV MENU -->
+			<nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+				<ul id="onepageNav" class="nav navbar-nav navbar-right"><!-- id="onepageNav" for homepage only -->
+					<li><a href="/" class='external'>HOME</a></li>
+				</ul>
+			</nav>
+			<!-- /NAV MENU -->
+
+		</div>
+	</header>
+	<!-- /HEADER -->
+
+	<div class='main-container' id='missing-div'>
+    	<img src="assets/images/magicwords.gif" id='missing-image'/>
+	</div>
   
 @stop
--- app/views/users/user_edit.blade.php
+++ app/views/users/user_edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.child')
+@extends('layouts.master')
 
 @section('tab-title')
 	<title>User Info</title>
@@ -6,6 +6,35 @@
 @stop
 
 @section('content')
+	<!-- HOME -->
+    <section id="home" class="homeTop nofull imageOnly color">
+        
+        <img src="/assets/images/preview/slider/MapGeoLocator.jpg" alt="Large Logo Image" id='large-logo'/>
+
+    </section>
+    <!-- /HOME -->
+
+    <!-- HEADER -->
+    <header id="header">
+        <div class="container main-container">
+
+            <!-- LOGO -->
+            <a href="#home" class="pull-left scrollTo"><img src="/assets/images/HotnColdLogo.png" height="60" alt="welcome" /></a>
+
+            <!-- MOBILE MENU -->
+            <button id="mobileMenu" class="fa fa-bars" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse"></button>
+
+            <!-- NAV MENU -->
+            <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+                <ul id="onepageNav" class="nav navbar-nav navbar-right"><!-- id="onepageNav" for homepage only -->
+                    <li><a href="/" class='external'>HOME</a></li>
+                </ul>
+            </nav>
+            <!-- /NAV MENU -->
+
+        </div>
+    </header>
+    <!-- /HEADER -->
 		@if ($edit)
 			<h1>Update User Info</h1>
     		{{ Form::open(array('action' => array('UsersController@update', $user->id), 'class' => 'form-horizontal', 'method' => 'put', 'files' => true)) }}
